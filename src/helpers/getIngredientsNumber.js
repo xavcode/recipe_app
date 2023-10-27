9665d6698111c17940a7d486432ab3e5b243afda
Javier Diaz <javiermauriciodiaz@gmail.com>
2023-10-26 20:16:04 -0500

    change the function that get the ingredients, to jus getting the index, in  order to use the same function to get the measures.

--- src/helpers/getIngredientsNumber.js
+++ src/helpers/getIngredientsNumber.js
@@ -1,11 +1,11 @@
-export default getIngredients = (data) => {
+export default getIngredientsNumber = (data) => {
   try {
     if (!data) return []
     const ingredients = []
     for (i = 1; i <= 20; i++) {
       const ingredient = data[`strIngredient${i}`]
       if (ingredient.trim()) {
-        ingredients.push(ingredient)
+        ingredients.push(i)
       }
     }
     // we use set to remove the repeated ingredients of the recipe
